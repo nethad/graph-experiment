8511e265aa93081f8f12f7f94107fe708c1842cd
Thomas Ritter <thomas.ritter@simplificator.com>
2017-04-06 16:11:54 +0200

    Clean up code, expose queries for view

--- app/controllers/api/data_controller.rb
+++ app/controllers/api/data_controller.rb
@@ -1,125 +1,122 @@
 class Api::DataController < ApplicationController
 
+  NEO4J_DATABASE_ENDPOINT = 'http://localhost:7474'
+
+  # QUERIES = {
+  #   tom_hanks_simple: '
+  #     MATCH (a:Actor)-[:ACTS_IN]-(m:Movie)
+  #     WHERE a.name = "Tom Hanks"
+  #     RETURN a, m
+  #     LIMIT 25
+  #   ',
+  #   tom_hanks_actor_director: '
+  #     MATCH
+  #     (a:Actor)-[:ACTS_IN]-(m:Movie),
+  #     (a:Actor)-[:DIRECTED]-(m:Movie)
+  #     WHERE a.name = "Tom Hanks"
+  #     RETURN a, m
+  #     LIMIT 25
+  #   ',
+  #   woody_allen_actor_director: '
+  #     MATCH
+  #     (a:Actor)-[:ACTS_IN]-(m:Movie),
+  #     (a:Actor)-[:DIRECTED]-(m:Movie)
+  #     WHERE a.name = "Woody Allen"
+  #     RETURN a, m
+  #     LIMIT 25
+  #   ',
+  #   mia_farrow: '
+  #     MATCH
+  #     (a:Director)-[:DIRECTED]-(m:Movie),
+  #     (b:Actor)-[:ACTS_IN]-(m:Movie)
+  #     WHERE a.name = "Woody Allen" AND b.name = "Mia Farrow"
+  #     RETURN a, b, m
+  #     LIMIT 25
+  #   ',
+  #   meg_ryan_shortest_path: '
+  #     MATCH p=shortestPath(
+  #       (bacon:Person {name:"Kevin Bacon"})-[*]-(meg:Person {name:"Meg Ryan"})
+  #     )
+  #     RETURN p
+  #   ',
+  #   kevin_bacon: '
+  #     MATCH (bacon:Person {name:"Kevin Bacon"})-[*1..3]-(hollywood)
+  #     RETURN DISTINCT hollywood
+  #   '
+  # }
+
+  # MATCH (a:Actor)-[:ACTS_IN]-(m:Movie)
+  # WHERE a.name = "Tom Hanks"
+  # RETURN a, m
+  # LIMIT 25
+  #
+  # MATCH
+  # (a:Actor)-[:ACTS_IN]-(m:Movie),
+  # (a:Actor)-[:DIRECTED]-(m:Movie)
+  # WHERE a.name = "Tom Hanks"
+  # RETURN a, m
+  # LIMIT 25
+  #
+  # MATCH
+  # (a:Actor)-[:ACTS_IN]-(m:Movie),
+  # (a:Actor)-[:DIRECTED]-(m:Movie)
+  # WHERE a.name = "Woody Allen"
+  # RETURN a, m
+  # LIMIT 25
+  #
+  # MATCH
+  # (a:Director)-[:DIRECTED]-(m:Movie),
+  # (b:Actor)-[:ACTS_IN]-(m:Movie)
+  # WHERE a.name = "Woody Allen" AND b.name = "Mia Farrow"
+  # RETURN a, b, m
+  # LIMIT 25
+  #
+  # MATCH p=shortestPath(
+  #   (bacon:Person {name:"Kevin Bacon"})-[*]-(meg:Person {name:"Meg Ryan"})
+  # )
+  # RETURN p
+  #
+  # MATCH (bacon:Person {name:"Kevin Bacon"})-[*1..3]-(hollywood)
+  # RETURN DISTINCT hollywood
+
   def show
-    # render json: json_data
-
-    # neo4j_session = Neo4j::Session.open(:server_db, 'http://localhost:7474')
-    # cypher = '
-    #   MATCH p=shortestPath(
-    #     (bacon:Person {name:"Kevin Bacon"})-[*]-(meg:Person {name:"Meg Ryan"})
-    #   )
-    #   RETURN p
-    # '
-    # render json: neo4j_session.query(cypher).to_json
-    #
-    #
-    # neo4j_session = Neo4j::Session.open(:server_db, 'http://localhost:7474')
-    # cypher = '
-    #   MATCH p=shortestPath(
-    #     (bacon:Person {name:"Kevin Bacon"})-[*]-(meg:Person {name:"Meg Ryan"})
-    #   )
-    #   RETURN p
-    # '
-    # neo4j_session.query(cypher)["data"].flatten
-    #
-    # 'http://localhost:7474/db/data/cypher'
-
-    conn = Faraday.new(:url => 'http://localhost:7474') do |faraday|
-      # faraday.request  :url_encoded             # form-encode POST params
-      faraday.response :logger                  # log requests to STDOUT
-      faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
-    end
-
-    # json_post = '
-    #   {
-    #     "query" : "MATCH (bacon:Person {name:\"Kevin Bacon\"})-[*1..3]-(hollywood) RETURN DISTINCT hollywood",
-    #     "params" : {}
-    #   }
-    # '
-
-    json_post =
-      {
-        "statements": [
-          {
-            "statement": "MATCH p=shortestPath(
-                (bacon:Person {name:\"Kevin Bacon\"})-[*]-(meg:Person {name:\"Meg Ryan\"})
-              )
-              RETURN p",
-            "resultDataContents": ["row", "graph"]
-          }
-        ]
-      }.to_json
-
-    resp = conn.post do |req|
-      # req.url '/db/data/cypher'
-      req.url '/db/data/transaction/commit'
-      req.headers['Content-Type'] = 'application/json'
-      req.body = json_post
-    end
-
-    render json: resp.body
+    response = execute_query(QUERIES[:meg_ryan_shortest_path])
+    render json: response.body
   end
-
-  # def show
-  #   neo4j_session = Neo4j::Session.open(:server_db, 'http://localhost:7474')
-  #   session.query(QUERY, json: json)
-  # end
 
   private
 
-  def json_data
-    '
-    {
-      "results": [
-          {
-              "columns": ["user", "entity"],
-              "data": [
-                  {
-                      "graph": {
-                          "nodes": [
-                              {
-                                  "id": "1",
-                                  "labels": ["User"],
-                                  "properties": {
-                                      "userId": "eisman"
-                                  }
-                              },
-                              {
-                                  "id": "8",
-                                  "labels": ["Project"],
-                                  "properties": {
-                                      "name": "neo4jd3",
-                                      "title": "neo4jd3.js",
-                                      "description": "Neo4j graph visualization using D3.js.",
-                                      "url": "https://eisman.github.io/neo4jd3"
-                                  }
-                              }
-                          ],
-                          "relationships": [
-                              {
-                                  "id": "7",
-                                  "type": "DEVELOPES",
-                                  "startNode": "1",
-                                  "endNode": "8",
-                                  "properties": {
-                                      "from": 1470002400000
-                                  }
-                              }
-                          ]
-                      }
-                  }
-              ]
-          }
-      ],
-      "errors": []
-    }
-    '
+  # def query
+  #   "MATCH p=shortestPath(
+  #       (bacon:Person {name:\"Kevin Bacon\"})-[*]-(meg:Person {name:\"Meg Ryan\"})
+  #     )
+  #     RETURN p"
+  # end
+
+  def connection
+    Faraday.new(url: NEO4J_DATABASE_ENDPOINT) do |faraday|
+      faraday.response :logger                  # log requests to STDOUT
+      faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+    end
   end
 
-  def json_data2
-    '
+  def neo4j_query_payload(query)
+    {
+      "statements": [
+        {
+          "statement": query,
+          "resultDataContents": ["row", "graph"]
+        }
+      ]
+    }.to_json
+  end
 
-    '
+  def execute_query(query)
+    connection.post do |req|
+      req.url '/db/data/transaction/commit'
+      req.headers['Content-Type'] = 'application/json'
+      req.body = neo4j_query_payload(query)
+    end
   end
 
 end
--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -1,7 +1,50 @@
 class GraphsController < ApplicationController
 
+  QUERIES = {
+    tom_hanks_simple: '
+      MATCH (a:Actor)-[:ACTS_IN]-(m:Movie)
+      WHERE a.name = "Tom Hanks"
+      RETURN a, m
+      LIMIT 25
+    ',
+    tom_hanks_actor_director: '
+      MATCH
+      (a:Actor)-[:ACTS_IN]-(m:Movie),
+      (a:Actor)-[:DIRECTED]-(m:Movie)
+      WHERE a.name = "Tom Hanks"
+      RETURN a, m
+      LIMIT 25
+    ',
+    woody_allen_actor_director: '
+      MATCH
+      (a:Actor)-[:ACTS_IN]-(m:Movie),
+      (a:Actor)-[:DIRECTED]-(m:Movie)
+      WHERE a.name = "Woody Allen"
+      RETURN a, m
+      LIMIT 25
+    ',
+    mia_farrow: '
+      MATCH
+      (a:Director)-[:DIRECTED]-(m:Movie),
+      (b:Actor)-[:ACTS_IN]-(m:Movie)
+      WHERE a.name = "Woody Allen" AND b.name = "Mia Farrow"
+      RETURN a, b, m
+      LIMIT 25
+    ',
+    meg_ryan_shortest_path: '
+      MATCH p=shortestPath(
+        (bacon:Person {name:"Kevin Bacon"})-[*]-(meg:Person {name:"Meg Ryan"})
+      )
+      RETURN p
+    ',
+    kevin_bacon: '
+      MATCH (bacon:Person {name:"Kevin Bacon"})-[*1..3]-(hollywood)
+      RETURN DISTINCT hollywood
+    '
+  }
+
   def index
-    # => render
+    @queries = QUERIES
   end
 
 end
